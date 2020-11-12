0abe8685d6205511dbd5972f3a13d6ea52e4c743
Konstantin Khabarlak <habarlack@gmail.com>
2020-11-12 13:11:16 +0200

    Complete omniglot 5way 1shot experiments

---
+++ k.khabarlak-maml/scipts/omniglot-5way-1shot.sh
@@ -0,0 +1,11 @@
+python train.py ../datasets/omniglot/ \
+    --dataset omniglot \
+    --num-ways 5 \
+    --num-shots 1 \
+    --use-cuda \
+    --step-size 0.4 \
+    --batch-size 32 \
+    --num-workers 8 \
+    --num-epochs 600 \
+    --verbose \
+    --output-folder ../results
