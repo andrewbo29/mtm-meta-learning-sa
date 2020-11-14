a2847497188889c7b60dbc3bd03781c4a5382484
Konstantin Khabarlak <habarlack@gmail.com>
2020-11-14 08:03:23 +0200

    Add omniglot 5-shot 5-way results

---
+++ k.khabarlak-maml/scipts/omniglot-5way-5shot.sh
@@ -0,0 +1,11 @@
+python train.py ../datasets/omniglot/ \
+    --dataset omniglot \
+    --num-ways 5 \
+    --num-shots 5 \
+    --use-cuda \
+    --step-size 0.4 \
+    --batch-size 25 \
+    --num-workers 8 \
+    --num-epochs 600 \
+    --verbose \
+    --output-folder ../results
