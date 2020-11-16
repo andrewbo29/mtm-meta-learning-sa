004b5f006aa49b22c55b80dced25746c32c1db16
Konstantin Khabarlak <habarlack@gmail.com>
2020-11-16 09:02:40 +0200

    Add miniimagenet training scripts

---
+++ k.khabarlak-maml/scripts/miniimagenet-5way-5shot-train.sh
@@ -0,0 +1,12 @@
+python ../src/train.py ../datasets/miniimagenet/ \
+    --dataset miniimagenet \
+    --num-ways 5 \
+    --num-shots 5 \
+    --num-steps 5 \
+    --step-size 0.1 \
+    --batch-size 2 \
+    --num-workers 4 \
+    --num-epochs 600 \
+    --use-cuda \
+    --verbose \
+    --output-folder ../results
