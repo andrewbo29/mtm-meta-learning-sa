3e83c63b01d34d3d2936b485c5d06584fac8b65b
Konstantin Khabarlak <habarlack@gmail.com>
2020-11-17 15:19:32 +0200

    Cifarfs and miniimagenet should use smaller hidden-size as per paper

--- k.khabarlak-maml/scripts/cifarfs-2way-5shot-train.sh
+++ k.khabarlak-maml/scripts/cifarfs-2way-5shot-train.sh
@@ -4,6 +4,7 @@ python ../src/train.py ../datasets/ \
     --num-shots 5 \
     --num-steps 5 \
     --step-size 0.1 \
+    --hidden-size 32 \
     --batch-size 4 \
     --num-workers 4 \
     --num-epochs 600 \
