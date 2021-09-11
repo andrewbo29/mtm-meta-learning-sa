d02977cde8c15b15c402edf611160769d7fd39a3
Ingvar-Y <39584137+Ingvar-Y@users.noreply.github.com>
2021-09-11 16:30:16 +0300

    additional fixes for device

--- i.yastrebov-dco/models/classification_heads.py
+++ i.yastrebov-dco/models/classification_heads.py
@@ -4,8 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch.autograd import Variable
 from qpth.qp import QPFunction
+from torch.autograd import Variable
 
 
 def computeGramMatrix(A, B):
